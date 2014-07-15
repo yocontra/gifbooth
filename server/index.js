9980931113aa434c2efd5ff9453f86129175e1ba
Contra <contra@maricopa.edu>
2014-07-14 19:09:29 -0700

    blacklisting

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ var sio = require('socket.io');
 var uuid = require('uuid');
 
 var rate = require('express-rate');
+var ipfilter = require('ipfilter');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var path = require('path');
@@ -15,14 +16,16 @@ var app = express();
 var server = http.Server(app);
 var io = sio(server);
 
+var banned = [];
+var maxVideos = 10;
+var counter = 0;
+
 var simpleMiddleware = rate.middleware({interval: 6, limit: 3});
 
+app.use(ipfilter(banned));
 app.use(express.static(path.join(__dirname, '../dist')));
 app.use(bodyParser());
 app.use(methodOverride());
-
-var maxVideos = 10;
-var counter = 0;
 
 del.sync(vidPath+'/*');
 
@@ -33,7 +36,9 @@ app.post('/upload', simpleMiddleware, function(req, res, next){
     counter = 0;
   }
   */
+  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   var outPath = path.join(vidPath, id+'.webm');
+  console.log(outPath, 'from', ip);
   req.pipe(fs.createWriteStream(outPath)).once('finish', function(){
     io.emit('video', id);
     res.status(200);
