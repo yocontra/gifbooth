fee5facde7617f3213b1dcc51bfffc5c9581b607
Contra <contra@maricopa.edu>
2014-07-14 18:22:33 -0700

    20

--- src/js/Application.js
+++ src/js/Application.js
@@ -36,7 +36,7 @@ var Application = React.createClass({
 
   addVideo: function(id) {
     var vids = this.state.videos;
-    if (this.state.videos.length >= 10) {
+    if (this.state.videos.length >= 20) {
       vids.pop();
     }
     vids.unshift({
