379bf18f252374981546d331ae021bb689497d09
Contra <contra@maricopa.edu>
2014-07-16 17:24:51 -0400

    fps fix

--- src/js/record.js
+++ src/js/record.js
@@ -5,33 +5,29 @@ module.exports = function(el, time, cb) {
   var w = el.videoWidth;
   var h = el.videoHeight;
   var encoder = new whammy.Video(60, 0.5);
-  //delete encoder.duration; // hack
+  delete encoder.duration; // hack
 
   var can = document.createElement('canvas');
   var vid = document.createElement('video');
   vid.src = el.src;
-  vid.play();
 
   var ctx = can.getContext('2d');
   can.width = w;
   can.height = h;
-  var start = Date.now();
+  var start = performance.now();
   var last = start;
 
   var grab = function(){
-    var now = Date.now();
+    var now = performance.now();
     var since = now-start;
     var duration = now-last;
-    if (duration === 0) {
-      duration = 1;
-    }
 
     if (since >= time) {
       return done();
     }
     ctx.drawImage(vid, 0, 0, w, h);
-    encoder.add(ctx);
-    last = Date.now();
+    encoder.add(ctx, duration);
+    last = performance.now();
     raf(grab);
   };
 
@@ -40,5 +36,6 @@ module.exports = function(el, time, cb) {
     cb(null, output);
   };
 
-  setTimeout(grab, 100);
+  vid.addEventListener('playing', grab, false);
+  vid.play();
 };
