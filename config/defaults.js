763ca5d0c5036e6561b7ddae97ab0fef0d95fe2b
Contra <contra@maricopa.edu>
2016-02-11 16:22:05 -0800

    https fix

--- config/defaults.js
+++ config/defaults.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "port": process.env.NODE_ENV === 'production' ? 80 : 9090,
+  "port": 9090,
   "banned": [],
   "backLog": 8,
   "duration": 2000,
--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
 var config = require('../config');
 var app = require('./connections/http');
 
-app.listen(config.get('port'), function(){
+var prod = process.env.NODE_ENV === 'production';
+
+var port = prod ? 443 : config.get('port');
+
+app.listen(port, function(){
   console.log('Listening on', config.get('port'));
 });
+
+if (prod) {
+  var http = require('http');
+  http.createServer(function (req, res) {
+    res.writeHead(301, { 'Location': 'https://' + req.headers['host'] + req.url });
+    res.end();
+  }).listen(80);
+}
 
 module.exports = app;
